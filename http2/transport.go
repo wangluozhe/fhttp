caffbda9b86e8bc9227e2d0b8eb44b6aaef9133f
LeeGene <1670044143@qq.com>
2023-05-12 16:55:17 +0800

    支持自定义http2指纹

--- http2/transport.go
+++ http2/transport.go
@@ -54,6 +54,14 @@ const (
 	defaultUserAgent = "Go-http-client/2.0"
 )
 
+// custom http2 settings
+type HTTP2Settings struct {
+	Settings       []Setting
+	ConnectionFlow int
+	HeaderPriority PriorityParam
+	PriorityFrames []PriorityFrame
+}
+
 // Transport is an HTTP/2 Transport.
 //
 // A Transport internally caches connections to servers. It is safe
@@ -71,6 +79,9 @@ type Transport struct {
 	// TLSClientConfig specifies the TLS configuration to use with
 	// tls.Client. If nil, the default configuration is used.
 	TLSClientConfig *tls.Config
+
+	// custom http2 settings
+	HTTP2Settings *HTTP2Settings
 
 	// ConnPool optionally specifies an alternate connection pool to use.
 	// If nil, the default is used.
@@ -745,7 +756,16 @@ func (t *Transport) newClientConn(c net.Conn, addr string, singleUse bool) (*Cli
 
 	cc.bw.Write(clientPreface)
 
-	if t.Navigator != "" {
+	if t.HTTP2Settings != nil {
+		inflowValue, nextStreamId, err := cc.fr.AutoWriteFrames(t)
+		if err != nil {
+			return nil, err
+		}
+
+		cc.inflow.add(int32(inflowValue))
+		cc.nextStreamID = nextStreamId
+
+	} else if t.Navigator != "" {
 		inflowValue, nextStreamId, err := cc.fr.AutoWriteFrames(t)
 		if err != nil {
 			return nil, err
@@ -839,25 +859,73 @@ func (t *Transport) ApplyPreset() {
 }
 
 func (fr *Framer) AutoWriteFrames(t *Transport) (inflowValue uint32, nextStreamId uint32, err error) {
-	switch t.Navigator {
-	case Firefox:
+	if t.HTTP2Settings != nil {
+		if t.HTTP2Settings.Settings != nil {
+			t.Settings = t.HTTP2Settings.Settings
+			for _, setting := range t.Settings {
+				if setting.ID == SettingInitialWindowSize {
+					inflowValue = setting.Val
+				}
+			}
+		} else if t.Navigator == Firefox {
+			inflowValue = 131072
+		} else if t.Navigator == Chrome {
+			inflowValue = 6291456
+		} else {
+			return 0, 0, errors.New(t.Navigator + " is not supported")
+		}
 		fr.WriteSettings(t.Settings...)
-		fr.WriteWindowUpdate(0, 12517377)
-		fr.WritePriority(3, PriorityParam{Weight: 200})
-		fr.WritePriority(5, PriorityParam{Weight: 100})
-		fr.WritePriority(7, PriorityParam{Weight: 0})
-		fr.WritePriority(9, PriorityParam{Weight: 0, StreamDep: 7})
-		fr.WritePriority(11, PriorityParam{Weight: 0, StreamDep: 3})
-		fr.WritePriority(13, PriorityParam{Weight: 240})
-		return 131072 + 12517377, 15, nil
+		var connectionFlow = uint32(0)
+		if t.HTTP2Settings.ConnectionFlow != 0 {
+			connectionFlow = uint32(t.HTTP2Settings.ConnectionFlow)
+		} else if t.Navigator == Firefox {
+			connectionFlow = uint32(12517377)
+		} else if t.Navigator == Chrome {
+			connectionFlow = uint32(15663105)
+		} else {
+			return 0, 0, errors.New(t.Navigator + " is not supported")
+		}
+		fr.WriteWindowUpdate(0, connectionFlow)
+		if t.HTTP2Settings.PriorityFrames != nil {
+			for _, frame := range t.HTTP2Settings.PriorityFrames {
+				fr.WritePriority(frame.StreamID, frame.PriorityParam)
+				nextStreamId = frame.StreamID + uint32(2)
+			}
+		} else if t.Navigator == Firefox {
+			fr.WritePriority(3, PriorityParam{Weight: 200})
+			fr.WritePriority(5, PriorityParam{Weight: 100})
+			fr.WritePriority(7, PriorityParam{Weight: 0})
+			fr.WritePriority(9, PriorityParam{Weight: 0, StreamDep: 7})
+			fr.WritePriority(11, PriorityParam{Weight: 0, StreamDep: 3})
+			fr.WritePriority(13, PriorityParam{Weight: 240})
+			nextStreamId = uint32(15)
+		} else if t.Navigator == Chrome {
+			nextStreamId = uint32(1)
+		} else {
+			return 0, 0, errors.New(t.Navigator + " is not supported")
+		}
+		return inflowValue + connectionFlow, nextStreamId, nil
+	} else {
+		switch t.Navigator {
+		case Firefox:
+			fr.WriteSettings(t.Settings...)
+			fr.WriteWindowUpdate(0, 12517377)
+			fr.WritePriority(3, PriorityParam{Weight: 200})
+			fr.WritePriority(5, PriorityParam{Weight: 100})
+			fr.WritePriority(7, PriorityParam{Weight: 0})
+			fr.WritePriority(9, PriorityParam{Weight: 0, StreamDep: 7})
+			fr.WritePriority(11, PriorityParam{Weight: 0, StreamDep: 3})
+			fr.WritePriority(13, PriorityParam{Weight: 240})
+			return 131072 + 12517377, 15, nil
 
-	case Chrome:
-		fr.WriteSettings(t.Settings...)
-		fr.WriteWindowUpdate(0, 15663105)
-		return 6291456 + 15663105, 1, nil
+		case Chrome:
+			fr.WriteSettings(t.Settings...)
+			fr.WriteWindowUpdate(0, 15663105)
+			return 6291456 + 15663105, 1, nil
 
-	default:
-		return 0, 0, errors.New(t.Navigator + " is not supported")
+		default:
+			return 0, 0, errors.New(t.Navigator + " is not supported")
+		}
 	}
 }
 
